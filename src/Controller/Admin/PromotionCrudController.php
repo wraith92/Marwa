b5fc32e5d2d62dc83390670cfbf19c1f374d965a
unknown <bensalahabdelrrahman1@gmail.com>
2023-05-20 18:56:20 +0200

    first commit

--- src/Controller/Admin/PromotionCrudController.php
+++ src/Controller/Admin/PromotionCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Promotion;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\PercentField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class PromotionCrudController extends AbstractCrudController
 {
@@ -12,14 +16,16 @@ class PromotionCrudController extends AbstractCrudController
         return Promotion::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IntegerField::new('remise')->formatValue(function ($value) {
+                return $value !== null ? $value . '%' : null;
+            }),
+            DateField::new('date_debut'),
+            DateField::new('date_fn'),
         ];
     }
-    */
+
 }
